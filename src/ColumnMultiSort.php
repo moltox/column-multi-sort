9f6466a95fd5af47b4e55b1c328c913621da44d1
Maik Müller <mueller@media-it-services.de>
2021-09-18 08:17:43 +0200

    Three step param finding working

--- src/ColumnMultiSort.php
+++ src/ColumnMultiSort.php
@@ -3,6 +3,7 @@
 namespace Moltox\ColumnMultiSort;
 
 use BadMethodCallException;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
@@ -10,21 +11,31 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Str;
-use Kyslik\ColumnSortable\Exceptions\ColumnMultiSortException;
+use Moltox\ColumnMultiSort\Exceptions\ColumnMultiSortException;
+use Moltox\ColumnMultiSort\Classes\SortQueries;
 
 trait ColumnMultiSort
 {
 
-    public function scopeMultiSort($query)
+    /**
+     * @param $query
+     * @param  array  $params
+     *
+     * Params: array(["column" => "direction", "column" => "direction"] )
+     *
+     * @return \Illuminate\Database\Query\Builder|mixed
+     * @throws ColumnMultiSortException
+     */
+    public function scopeMultiSort($query, array $params = [])
     {
 
-        $sorts = $this->findParams();
+        $sorts = $this->findParams($params);
 
         Log::debug('ColumnMultiSort'.print_r($sorts, true));
 
         if (!empty($sorts)) {
 
-            return $this->queryOrderBuilder($query, $sorts);
+            return $this->buildQueries($query, $sorts);
 
         }
 
@@ -32,20 +43,16 @@ trait ColumnMultiSort
 
     }
 
-    private function findParams()
+    /**
+     * Attempt to find params with priority:
+     * 1. parameters in request
+     * 2. parameters as on scope command
+     * 3. parameters set as default in model
+     *
+     * @return array|mixed
+     */
+    private function findParams($params = [])
     {
-
-        $sorts = [];
-
-        return $this->readParamsFromRequest();
-
-
-    }
-
-    private function readParamsFromRequest()
-    {
-
-        $sorts = [];
 
         if (request()->has('order')) {
 
@@ -53,129 +60,57 @@ trait ColumnMultiSort
 
         }
 
-        return $sorts;
+        if (!empty($params) ) {
+
+            return $params;
+
+        }
+
+        if (isset($this->sortDefault) && !empty($this->sortDefault)) {
+
+            return $this->sortDefault;
+
+        }
 
     }
 
     /**
-     * @param  \Illuminate\Database\Query\Builder  $query
-     * @param  array  $sortColumnParams
+     * @param  Builder  $query
+     * @param  array  $sorts
      *
-     * @return \Illuminate\Database\Query\Builder
-     *
-     * @throws ColumnMultiSortException
+     * @return Builder
      */
-    private function queryOrderBuilder($query, array $sortColumnParams)
+    private function buildQueries(Builder $query, array $sorts)
     {
 
-        /**
-         * @var Model $model
-         */
-        $model = $this;
+        Log::debug('buildQueries: '.print_r([$query::class, $sorts], true));
 
-        foreach ($sortColumnParams as $column => $direction) {
-
+        foreach ($sorts as $column => $direction) {
+            Log::debug('buildQueries 1: '.print_r([$sorts, $column, $direction], true));
             if ($this->isRelational($column)) {
-                // TODO add relational
-                Log::debug('is relational: '.$column);
-                unset($sortColumnParams[$column]);
-            }
 
-         //   Log::debug("One col:".print_r(['arr' => $sortColumnParams, 'foo' => ['col' => $column, 'dir' => $direction]], true));
+                $splitted = $this->splitRelated($column);
 
-            if (isset($model->sortableAs) && in_array($column, $model->sortableAs)) {
+                $relation = $splitted[0];
+                // TODO add handle for multi relation
+                $column = $splitted[count($splitted) - 1];
 
-                Log::debug('Sortable as col: ' . $column);
-                $query = $query->orderBy($column, $direction);
+              //  $query = SortQueries::addJoin($query, $relation, $column);
 
-            } elseif ($this->isSortableColumn($model, $column)) {
+            } else {
 
-                $column = $model->getTable().'.'.$column;
-                Log::debug('Add query for (table.col): ' . $column);
-                $query = $query->orderBy($column, $direction);
+                $query->orderBy($column, $direction);
 
             }
+
+
+
         }
 
-
+        Log::debug('Query ' . $query->toSql());
 
         return $query;
 
-        if (is_null($column)) {
-            return $query;
-        }
-
-
-
-        if (!empty($relationColumnArray)) {
-            $relationName = $relationColumnArray[0];
-            $column = $relationColumnArray[1];
-
-            try {
-
-                $relation = $query->getRelation($relationName);
-
-                $query = $this->queryJoinBuilder($query, $relation);
-
-            } catch (BadMethodCallException $e) {
-                throw new ColumnMultiSortException($relationName, 1, $e);
-            } catch (\Exception $e) {
-                throw new ColumnMultiSortException($relationName, 2, $e);
-            }
-
-            $model = $relation->getRelated();
-        }
-
-
-
-    }
-
-
-    /**
-     * @param  \Illuminate\Database\Query\Builder  $query
-     * @param  \Illuminate\Database\Eloquent\Relations\BelongsTo|\Illuminate\Database\Eloquent\Relations\HasOne  $relation
-     *
-     * @return \Illuminate\Database\Query\Builder
-     *
-     * @throws \Exception
-     */
-    private function queryJoinBuilder($query, $relation)
-    {
-
-        $relatedTable = $relation->getRelated()->getTable();
-        $parentTable = $relation->getParent()->getTable();
-
-        if ($parentTable === $relatedTable) {
-            $query = $query->from($parentTable.' as parent_'.$parentTable);
-            $parentTable = 'parent_'.$parentTable;
-            $relation->getParent()->setTable($parentTable);
-        }
-
-        if ($relation instanceof HasOne) {
-            $relatedPrimaryKey = $relation->getQualifiedForeignKeyName();
-            $parentPrimaryKey = $relation->getQualifiedParentKeyName();
-        } elseif ($relation instanceof BelongsTo) {
-            $relatedPrimaryKey = $relation->getQualifiedOwnerKeyName();
-            $parentPrimaryKey = $relation->getQualifiedForeignKeyName();
-        } else {
-            throw new \Exception();
-        }
-
-        return $this->formJoin($query, $parentTable, $relatedTable, $parentPrimaryKey, $relatedPrimaryKey);
-    }
-
-
-    /**
-     * @param $model
-     * @param $column
-     *
-     * @return bool
-     */
-    private function isSortableColumn($model, $column)
-    {
-
-        return (isset($model->sortable)) ? in_array($column, $model->sortable) :
-            Schema::connection($model->getConnectionName())->hasColumn($model->getTable(), $column);
     }
 
     /**
@@ -186,73 +121,15 @@ trait ColumnMultiSort
     private function isRelational(string $column)
     {
 
-        return count(explode(config('column-multi-sort.uri_relation_column_separator', '.'), $column)) > 1;
+        return count($this->splitRelated($column)) > 1;
 
     }
 
-
-    /**
-     * @param  array|string  $array
-     *
-     * @return array
-     */
-    private function formatToParameters($array)
+    private function splitRelated($relationColumn)
     {
 
-        if (empty($array)) {
-            return [];
-        }
+        return explode(config('column-multi-sort.uri_relation_column_separator', '.'), $relationColumn);
 
-        $defaultDirection = config('column-multi-sort.default_direction', 'asc');
-
-        if (is_string($array)) {
-            return ['sort' => $array, 'direction' => $defaultDirection];
-        }
-
-        return (key($array) === 0) ? ['sort' => $array[0], 'direction' => $defaultDirection] : [
-            'sort' => key($array),
-            'direction' => reset($array),
-        ];
-    }
-
-
-    /**
-     * @param $query
-     * @param $parentTable
-     * @param $relatedTable
-     * @param $parentPrimaryKey
-     * @param $relatedPrimaryKey
-     *
-     * @return mixed
-     */
-    private function formJoin($query, $parentTable, $relatedTable, $parentPrimaryKey, $relatedPrimaryKey)
-    {
-
-        $joinType = config('column-multi-sort.join_type', 'leftJoin');
-
-        return $query->select($parentTable.'.*')
-            ->{$joinType}($relatedTable, $parentPrimaryKey, '=', $relatedPrimaryKey);
-    }
-
-    private function explodeSortParameter($parameter): array
-    {
-
-        $separator = config('column-multi-sort.uri_relation_column_separator', '.');
-
-        if (Str::contains($parameter, $separator)) {
-
-            $oneToOneSort = explode($separator, $parameter);
-
-            if (count($oneToOneSort) !== 2) {
-
-                throw new ColumnMultiSortException();
-
-            }
-
-            return $oneToOneSort;
-        }
-
-        return [];
     }
 
 }
